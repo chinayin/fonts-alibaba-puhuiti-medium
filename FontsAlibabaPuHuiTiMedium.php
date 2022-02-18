f19a4771c033c58b346f89bc64cd46e1052d82c2
lei.tian <whereismoney@qq.com>
2022-02-18 18:05:43 +0800

    更新字体库

---
+++ FontsAlibabaPuHuiTiMedium.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fonts\AlibabaPuHuiTi\Medium;
+
+class FontsAlibabaPuHuiTiMedium
+{
+
+    /**
+     * @return string
+     */
+    public static function getPath(): string
+    {
+        return __DIR__ . 'Alibaba-PuHuiTi-Medium.ttf';
+    }
+
+
+}
